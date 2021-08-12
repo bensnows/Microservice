2fb1d95b069d3c63f241324dfd12fe4056f0a8e8
陳新 <hsin.chen@iisigroup.com>
2021-08-12 19:31:26 +0800

    add code

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const fs = require("fs")
+const app = express()
+
+if(!process.env.PORT){
+    throw new Error("Please specift the prot number for the HTTP server with the environment variable PORT. ");
+}
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get("/video",(req,res)=>{
+    const path = "../video/SampleVideo_1280x720_1mb.mp4";
+    fs.stat(path,(err, stats)=>{
+        if(err){
+            console.error("Error occurred");
+            console.log(err)
+            res.sendStatus(500);
+            return;
+        }
+        res.writeHead(200,{
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4"
+        })
+        fs.createReadStream(path).pipe(res);
+    })
+    
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
