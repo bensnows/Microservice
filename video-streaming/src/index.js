fda2f43b4421fa981a1e5ed9e47c2d2e005c5c7f
陳新 <hsin.chen@iisigroup.com>
2021-08-16 00:14:14 +0800

    update video-streaming project, retrieve image from video-storage service

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,38 +1,49 @@
-const express = require('express')
-const fs = require("fs")
-const app = express()
+const express = require("express");
+const http = require("http");
+const fs = require("fs");
+const app = express();
 
-if(!process.env.PORT){
-    throw new Error("Please specift the prot number for the HTTP server with the environment variable PORT. ");
+if (!process.env.PORT) {
+  throw new Error(
+    "Please specift the port number for the HTTP server with the environment variable PORT. "
+  );
+}
+
+if (!process.env.VIDEO_STORAGE_HOST) {
+  throw new Error("Video storage host is lacked");
+}
+
+if (!process.env.VIDEO_STORAGE_PORT || isNaN(process.env.VIDEO_STORAGE_PORT)) {
+  throw new Error("Video storage host is lacked");
 }
 
 const port = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-
-app.get("/video",(req,res)=>{
-    const path = "./videos/SampleVideo_1280x720_1mb.mp4";
-    fs.stat(path,(err, stats)=>{
-        if(err){
-            console.error("Error occurred");
-            console.log(err)
-            res.sendStatus(500);
-            return;
-        }
-        res.writeHead(200,{
-            "Content-Length": stats.size,
-            "Content-Type": "video/mp4"
-        })
-        fs.createReadStream(path).pipe(res);
-    })
-    
-})
-
+app.get("/video", (req, res) => {
+  const fileName = "SampleVideo_1280x720_1mb.mp4";
+  const forwardRequest = http.request(
+    {
+      host: VIDEO_STORAGE_HOST,
+      port: VIDEO_STORAGE_PORT,
+      path: `/video?path=${fileName}`,
+      method: "GET",
+      headers: req.headers,
+    },
+    (forwardRes) => {
+      res.writeHeader(forwardRes.statusCode, forwardRes.headers);
+      forwardRes.pipe(res);
+    }
+  );
+  req.pipe(forwardRequest);
+});
 
 app.listen(port, () => {
-  console.log(`HelloWorld listening at http://<docker_host>:${port}`)
-  console.log(`Example image listening at http://<docker_host>:${port}/videos`)
-})
+  console.log(`HelloWorld listening at http://<docker_host>:${port}`);
+  console.log(`Example image listening at http://<docker_host>:${port}/videos`);
+});
