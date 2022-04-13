7779f32579b544b94abb88bcb1c73d92f369db8b
陳新(Hsin Chen) <1909002@iisigroup.com>
2022-04-13 23:24:52 +0800

    add mongoDB

--- video-streaming/src/index.js
+++ video-streaming/src/index.js
@@ -1,49 +1,71 @@
 const express = require("express");
 const http = require("http");
-const fs = require("fs");
+// const fs = require("fs");
+const mongodb = require("mongodb");
+
 const app = express();
 
+
 if (!process.env.PORT) {
-  throw new Error(
-    "Please specift the port number for the HTTP server with the environment variable PORT. "
-  );
+    throw new Error(
+        "Please specift the port number for the HTTP server with the environment variable PORT. "
+    );
 }
 
 if (!process.env.VIDEO_STORAGE_HOST) {
-  throw new Error("Video storage host is lacked");
+    throw new Error("Video storage host is lacked");
 }
 
 if (!process.env.VIDEO_STORAGE_PORT || isNaN(process.env.VIDEO_STORAGE_PORT)) {
-  throw new Error("Video storage host is lacked");
+    throw new Error("Video storage host is lacked");
 }
 
 const port = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+function main() {
+    return mongodb.MongoClient.connect(DBHOST).then(client => {
+        const db = client.db(DBNAME);
+        const videosCollection = db.collection("videos");
 
-app.get("/video", (req, res) => {
-  const fileName = "SampleVideo_1280x720_1mb.mp4";
-  const forwardRequest = http.request(
-    {
-      host: VIDEO_STORAGE_HOST,
-      port: VIDEO_STORAGE_PORT,
-      path: `/video?path=${fileName}`,
-      method: "GET",
-      headers: req.headers,
-    },
-    (forwardRes) => {
-      res.writeHeader(forwardRes.statusCode, forwardRes.headers);
-      forwardRes.pipe(res);
-    }
-  );
-  req.pipe(forwardRequest);
-});
+        app.get("/video", (req, res) => {
+            const videoId = new mongodb.ObjectID(req.query.id);
 
-app.listen(port, () => {
-  console.log(`HelloWorld listening at http://<docker_host>:${port}`);
-  console.log(`Example image listening at http://<docker_host>:${port}/videos`);
-});
+            videosCollection
+                .findOne({ _id: videoId })
+                .then(videoRecord => {
+                    const forwardRequest = http.request({
+                            host: VIDEO_STORAGE_HOST,
+                            port: VIDEO_STORAGE_PORT,
+                            path: `/video?path=${videoRecord.videoPath}`,
+                            method: "GET",
+                            headers: req.headers,
+                        },
+                        (forwardRes) => {
+                            res.writeHeader(forwardRes.statusCode, forwardRes.headers);
+                            forwardRes.pipe(res);
+                        }
+                    );
+                    req.pipe(forwardRequest);
+                }).catch(err => {
+                    console.error("Database query failed.");
+                    console.error(err && err.stack || err);
+                    res.sendStatus(500);
+                })
+
+        });
+        app.listen(port, () => {
+            console.log(`Microservice online.`);
+        });
+    });
+}
+
+main()
+    .then(() => console.log("Microservice online."))
+    .catch(err => {
+        console.error("Microservice failed to start.");
+        console.error(err && err.stack || err);
+    });
